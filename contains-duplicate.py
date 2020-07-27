ad7435651f5aa228d1d00728e8545e2534ce2050
Mark Gerrior <github@gerrior.com>
2020-07-27 14:50:55 -0700

    contains-duplicate solution

---
+++ contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dictionary = {}
+        
+        for digit in nums:
+            if digit in dictionary:
+                dictionary[digit] = True 
+            else:
+                dictionary[digit] = False 
+
+        for key, value in dictionary.items():
+            if value == True:
+                return True
+        
+        return False
