871c72af6c50c4c4568d72f859cd1e9be97274a3
Mark Gerrior <github@gerrior.com>
2020-07-31 12:09:57 -0700

    Init commit

---
+++ search-in-rotated-sorted-array.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        return 69
+
+a = Solution()
+print(a.search([4,5,6,7,0,1,2], 0))
