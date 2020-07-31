211fef7d4727c4bde9959b45078d2da8cfa23c84
Mark Gerrior <github@gerrior.com>
2020-07-31 12:13:31 -0700

    Unoptimized solution

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -2,7 +2,18 @@
 
 class Solution:
     def search(self, nums: [int], target: int) -> int:
-        return 69
+        index = -1
+
+        for num in nums:
+            index += 1
+            if num == target:
+                return index
+
+        return -1
 
 a = Solution()
+# 4
 print(a.search([4,5,6,7,0,1,2], 0))
+# -1
+print(a.search([4,5,6,7,0,1,2], 3))
+
